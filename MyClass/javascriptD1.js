41a269b81e0c5e89f8f2ea232687378702cdd966
123aleqsandre <aleqsandremrelashvili@gmail.com>
2026-01-22 20:01:19 +0400

    Update javascriptD1.js

--- MyClass/javascriptD1.js
+++ MyClass/javascriptD1.js
@@ -1,2 +1,11 @@
 let grade = prompt("ur grade?");
-alert(grade / 10);
+let grade1 = grade / 10;
+const num = 5;
+const num1 = 9;
+if (grade1 >= 9) {
+  alert("u passed the test very good");
+} else if (5 > grade1 < 9) {
+  alert("chaabaret mara nichivo ra");
+} else {
+  alert("chaiwerit meored agar moxvidet");
+}
