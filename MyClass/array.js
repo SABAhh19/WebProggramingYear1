f8fb9bf4aa5afb57ba762b9745b411487d25056f
123aleqsandre <aleqsandremrelashvili@gmail.com>
2026-02-05 19:49:47 +0400

    s

--- MyClass/array.js
+++ MyClass/array.js
@@ -1,5 +1,5 @@
-const numbers = [12, 7, 6, 92, 64, 37];
-for (let i = 0; i < array.length; i++) {
+let numbers = [12, 7, 6, 92, 64, 37];
+for (let i = 0; i < numbers.length; i++) {
   if (numbers[i] % 2 === 0) {
     console.log(numbers[i]);
   }
---
+++ MyClass/train.js
@@ -0,0 +1,25 @@
+// const numbers = [70, 85, 90, 60, 75];
+// let sum = 0;
+// let sum1 = 0;
+// for (let i = 0; i < numbers.length; i++) {
+//   sum = sum + 1;
+//   sum1 = sum1 + numbers[i];
+// }
+// console.log(sum1 / sum);
+
+// const numbers = [10, 25, 8, 30, 15];
+// for (let i = 0; i < numbers.length; i++) {
+//   if (numbers[0] < numbers[i]) {
+//     console.log(numbers[i]);
+//   }
+// }
+
+const names = ["SABAhh", "mamacita"];
+const password = ["SabaSaba37", "mamacita777"];
+let name = prompt("name?");
+let pass = prompt("password?");
+for (let i = 0; i < names.length; i++) {
+  if (name === names[i] && pass === password[i]) {
+    alert("u succsessfully loged in");
+  }
+}
