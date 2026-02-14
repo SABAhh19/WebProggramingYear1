5beedd1e111b8f6158f97287f5904e98935c65a6
SABAhh19 <sabaletodiani06@gmail.com>
2026-02-14 20:17:10 +0400

    objects training

---
+++ HomeWorks/objectTraining.js
@@ -0,0 +1,53 @@
+// const products = [
+//   {
+//     name: "Headphones",
+//     price: 120,
+//     discount: 20,
+//     inStock: true,
+//   },
+//   {
+//     name: "Keyboard",
+//     price: 80,
+//     discount: 10,
+//     inStock: false,
+//   },
+//   {
+//     name: "Mouse",
+//     price: 40,
+//     discount: 5,
+//     inStock: true,
+//   },
+//   {
+//     name: "Monitor",
+//     price: 300,
+//     discount: 50,
+//     inStock: true,
+//   },
+// ];
+// for (let i = 0; i < products.length; i++) {
+//   if (products[i].inStock === true) {
+//     const finalPrice = products[i].price * (1 - products[i].discount / 100);
+//     console.log(
+//       products[i].name,
+//       " : final price after ",
+//       products[i].discount,
+//       "% discount is ",
+//       finalPrice,
+//     );
+//   }
+// }
+
+const cart = {
+  user: "Ana",
+  items: ["Headphones", "Mouse", "Keyboard"],
+  prices: [120, 40, 80],
+};
+let sum = 0;
+
+for (let i = 0; i < cart.items.length; i++) {
+  console.log(cart.items[i]);
+}
+for (let i = 0; i < cart.prices.length; i++) {
+  sum = sum + cart.prices[i];
+}
+console.log("total price is ", sum);
