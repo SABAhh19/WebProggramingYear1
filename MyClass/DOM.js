7da87d9bef578848ce1fe4f582519310c6976f37
123aleqsandre <aleqsandremrelashvili@gmail.com>
2026-02-19 19:53:35 +0400

    rom

---
+++ MyClass/DOM.js
@@ -0,0 +1,16 @@
+// const header = document.getElementById("header");
+// const para1 = document.getElementById("para1");
+// const para2 = document.getElementById("para2");
+
+// header.innerHTML = "Hello World";
+// para1.innerHTML = "paragrapi1";
+// para2.innerHTML = "paragrapui2";
+
+const paragraps = document.querySelectorAll("p");
+for (let i = 0; i < paragraps.length; i++) {
+  paragraps[i].innerHTML = "saba";
+  paragraps[i].style.border = "3px solid black";
+  paragraps[i].style.background = "red";
+  paragraps[i].style.color = "green";
+  paragraps[i].style.margin = "10px";
+}
