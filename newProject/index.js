42127255c59cef9dddaddcddea7653f530be8019
SABAhh19 <sabaletodiani06@gmail.com>
2026-03-18 13:20:23 +0400

    api

--- newProject/index.js
+++ newProject/index.js
@@ -3,6 +3,13 @@ const items = [
   { item: "Margiela Gats", price: "$149.99" },
   { item: "Levis jeans", price: "$89.99" },
   { item: "Zara Jacket", price: "$119.99" },
+  { item: "Valentino", price: "$159.99" },
 ];
 
-for (let i = 0; i < items.length; i++) {}
+const names = document.getElementsByClassName("product-name");
+const prices = document.getElementsByClassName("price");
+
+for (let i = 0; i < items.length; i++) {
+  names[i].innerHTML = items[i].item;
+  prices[i].innerHTML = items[i].price;
+}
