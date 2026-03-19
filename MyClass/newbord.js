e55973a310961fbf83b8a6a65da9ec65f491dcba
123aleqsandre <aleqsandremrelashvili@gmail.com>
2026-03-19 16:23:49 +0400

    sad

---
+++ MyClass/newbord.js
@@ -0,0 +1,20 @@
+// const btn = document.getElementById("btn");
+// const box = document.getElementById("container");
+
+// btn.addEventListener("click", function () {
+//   box.classList.add("show");
+// });
+
+// btn.addEventListener("click", function () {
+//   box.classList.remove("show");
+// });
+
+const parentbox = document.querySelector(".container");
+const littlebox = document.createElement("div");
+const img = document.createElement("img");
+const link = document.createElement("a");
+littlebox.setAttribute("");
+littlebox.setAttribute("");
+littlebox.classList.add(".little-box");
+parentbox.appendChild(littlebox);
+const body = document.body;
--- newProject/index.js
+++ newProject/index.js
@@ -8,8 +8,10 @@ const items = [
 
 const names = document.getElementsByClassName("product-name");
 const prices = document.getElementsByClassName("price");
+const stock = document.getElementsByClassName("meta");
 
 for (let i = 0; i < items.length; i++) {
   names[i].innerHTML = items[i].item;
   prices[i].innerHTML = items[i].price;
+  stock[i].innerHTML = items[i].inStock;
 }
