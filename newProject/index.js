b7f2f0d1643d8bd5d7c75656b849b6fb9d6ba769
123aleqsandre <aleqsandremrelashvili@gmail.com>
2026-03-26 18:44:37 +0400

    newproject

--- newProject/index.js
+++ newProject/index.js
@@ -7,8 +7,6 @@ const items = [
 
 const productsGrid = document.querySelector(".products-grid");
 
-productsGrid.innerHTML = "";
-
 for (let i = 0; i < items.length; i++) {
   let badge = "";
   if (i === 0) {
@@ -34,13 +32,4 @@ for (let i = 0; i < items.length; i++) {
       </div>
     </div>
   `;
-}
-const names = document.getElementsByClassName("product-name");
-const prices = document.getElementsByClassName("price");
-const stock = document.getElementsByClassName("meta");
-
-for (let i = 0; i < items.length; i++) {
-  names[i].innerHTML = items[i].item;
-  prices[i].innerHTML = items[i].price;
-  stock[i].innerHTML = items[i].inStock;
 }
