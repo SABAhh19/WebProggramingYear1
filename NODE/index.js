4f516ad20c16ffcbb37a70f7e43c4fb060aefd85
SABAhh19 <sabaletodiani06@gmail.com>
2026-04-28 20:01:56 +0400

    asdas

--- NODE/index.js
+++
@@ -1,6 +0,0 @@
-const http = require("http");
-const server = http.createServer((request, response) => {
-  response.write("Hello World");
-  response.end();
-});
-server.listenerCount(3000);
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,19 @@
-import express from "express";
+import express, { response } from "express";
 
 const app = express();
+
+app.use(express.json());
 
 const users = [
   { id: 1, username: "saba", age: 16 },
   { id: 2, username: "saba shav", age: 17 },
   { id: 3, username: "niko", age: 20 },
+];
+
+const prod = [
+  { id: 1, name: "saba", pr: 16 },
+  { id: 2, name: "saba shav", pr: 17 },
+  { id: 3, name: "niko", pr: 20 },
 ];
 
 app.get("/users", (req, res) => {
@@ -17,6 +25,18 @@ app.get("/users", (req, res) => {
   }
 
   res.status(200).send(users);
+});
+
+app.post("/users", (req, res) => {
+  const body = req.body;
+  const newId = users[users.length - 1].id + 1;
+  const newUser = {
+    id: newId,
+    username: body.username,
+    password: body.password,
+  };
+  users.push(newUser);
+  res.send(users);
 });
 
 app.listen(3000, () => {
