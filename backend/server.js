cfd87f39257000839b04a2eb91da003f7f127f7a
SABAhh19 <sabaletodiani06@gmail.com>
2026-04-30 15:02:08 +0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,41 +4,23 @@ const app = express();
 
 app.use(express.json());
 
-const users = [
-  { id: 1, username: "saba", age: 16 },
-  { id: 2, username: "saba shav", age: 17 },
-  { id: 3, username: "niko", age: 20 },
+const products = [
+  { id: 1, name: "keyboard", price: 120 },
+  { id: 2, name: "monitor", price: 220 },
+  { id: 3, name: "desk", price: 180 },
+  { id: 4, name: "phone", price: 370 },
 ];
 
-const prod = [
-  { id: 1, name: "saba", pr: 16 },
-  { id: 2, name: "saba shav", pr: 17 },
-  { id: 3, name: "niko", pr: 20 },
-];
+app.get("/products", (req, res) => {
+  res.send(products);
+});
 
-app.get("/users", (req, res) => {
-  const { username } = req.query;
-
-  if (username) {
-    const filter = users.filter((user) => user.username === username);
-    return res.status(200).send(filter);
+app.get("/products/:id", (req, res) => {
+  const parsedId = parseInt(req.params.id);
+  if (isNaN(parsedId)) {
+    return res.status(400).send({ msg: "bad request give good id" });
   }
-
-  res.status(200).send(users);
-});
-
-app.post("/users", (req, res) => {
-  const body = req.body;
-  const newId = users[users.length - 1].id + 1;
-  const newUser = {
-    id: newId,
-    username: body.username,
-    password: body.password,
-  };
-  users.push(newUser);
-  res.send(users);
-});
-
-app.listen(3000, () => {
-  console.log("app is listening at http://localhost:3000");
+  const findProd = products.find((user) => user.id === parsedId);
+  if (!findProd) return res.sendStatus(404);
+  return res.send(findProd);
 });
